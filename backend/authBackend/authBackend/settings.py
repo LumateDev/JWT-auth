5f5587a7162bda12febe95254e44262dedfbeebd
Lumate <sanchous557@mail.ru>
2024-04-24 20:04:36 +0300

    update settings, add env params

--- backend/authBackend/authBackend/settings.py
+++ backend/authBackend/authBackend/settings.py
@@ -177,3 +177,13 @@ DJOSER = {
         'user_delete': "djoser.serializers.UserDeleteSerializer",
     },
 }
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = env("EMAIL_HOST")
+EMAIL_USE_TLS = True
+EMAIL_PORT = env("EMAIL_PORT")
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = "info@jwt-auth.com"
+DOMAIN = env("DOMAIN")
+SITE_NAME = "JWT auth"
